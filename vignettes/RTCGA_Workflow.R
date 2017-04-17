3c252a101c18c6f078c0a58c3ec0c9d56c5dffc2
Herve Pages <hpages@fhcrc.org>
2017-04-17 19:24:16 +0000

    Processed vignettes (PDF or HTML) don't belong to the package source tree and are actually harmful.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RTCGA@128787 bc3139a8-67e5-0310-9ffc-ced21a209358

--- vignettes/RTCGA_Workflow.R
+++
@@ -1,4 +0,0 @@
-## ---- echo=FALSE------------------------------------------------------------------------------------------------------------------------------------
-library(knitr)
-opts_chunk$set(comment="", message=FALSE, warning = FALSE, tidy.opts=list(keep.blank.line=TRUE, width.cutoff=150),options(width=150))
-
